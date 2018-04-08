d8bca753c5c9493eee7f67131ea7d4f147177c66
Arong Wu <awuit9090@gmail.com>
2018-04-08 15:41:43 -0400

    v2

--- app.js
+++ app.js
@@ -1,17 +1,31 @@
 const express = require('express');
 const bodyParser = require('body-parser');
+const MongoClient = require('mongodb').MongoClient
 const app = express();
 
-app.use(bodyParser.urlencoded({extended: true}))
+var db
 
-app.listen(3000, function() {
-	console.log('listening on 3000')
-})
+MongoClient.connect('mongodb://awu:save%24800K@ds237669.mlab.com:37669/quotes', (err, client) => {
+	if (err) return console.log(err)
+
+	db = client.db('quotes')
+
+	app.listen(3000, function() {
+		console.log('listening on 3000')
+	})
+})	
+
+app.use(bodyParser.urlencoded({extended: true}))
 
 app.get('/', (req, res) => {
 	res.sendFile(__dirname + '/index.html')
 })
 
 app.post('/quotes', (req, res) => {
-	console.log(req.body)
+	db.collection('quotes').save(req.body, (err, result) => {
+		if (err) return console.log(err)
+
+		console.log('Saved to Database')
+		res.redirect('/')
+	})
 })
